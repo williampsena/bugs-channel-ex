9dd073ddfc757f5547bfbd093c8ed0c3d37859a8
William Sena <me@willsena.dev>
2023-09-26 21:32:22 -0300

    fix redundant is_nil

--- lib/plugins/sentry/plugs/auth_key.ex
+++ lib/plugins/sentry/plugs/auth_key.ex
@@ -25,11 +25,9 @@ defmodule BugsChannel.Plugins.Sentry.Plugs.AuthKey do
   def feth_project_auth_key(conn) do
     sentry_auth_header = get_req_header(conn, "x-sentry-auth") |> List.first()
 
-    if is_nil(sentry_auth_header) do
-      nil
-    else
+    if is_binary(sentry_auth_header) do
       match = ~r/sentry_key=(?<value>.[^,]+)/ |> Regex.named_captures(sentry_auth_header)
-      if is_nil(match), do: nil, else: match["value"]
+      if is_map(match), do: match["value"]
     end
   end
 end
